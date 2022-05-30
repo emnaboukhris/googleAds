18efa24d5dcae60f064bb8e321ee69eea49e6f91
emnaboukhris <emnaboukhris@insat.u-carthage.tn>
2022-05-30 17:30:41 +0200

    GoogleMaps

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:googleads/screens/map/Location.dart';
+import 'package:googleads/screens/map/follow_location.dart';
+import 'package:googleads/screens/map/polyline.dart';
+import 'package:googleads/services/geolocator_service.dart';
 import 'package:googleads/wrapper.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -17,21 +23,28 @@ class MyApp extends StatelessWidget {
 
   Color _primaryColor = HexColor('#80FF72');
   Color _accentColor = HexColor('#7EE8FA') ;
+  final geoService = GeolocatorService();
 
   // This widget is the root of your application.
 
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primaryColor: _primaryColor,
-          accentColor: _accentColor ,
-          scaffoldBackgroundColor : Colors.grey.shade100 ,
-          primarySwatch: Colors.grey ,
+    return FutureProvider(
+      create: (context) => geoService.getInitialLocation(),
+      initialData: null,
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primaryColor: _primaryColor,
+            accentColor: _accentColor ,
+            scaffoldBackgroundColor : Colors.grey.shade100 ,
+            primarySwatch: Colors.grey ,
 
-        ),
-        home:Wrapper(title: '',)  );
+          ),
+
+          home:Polyline(),
+      ),
+    );
   }
 }
